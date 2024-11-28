71cf56c45e65779a81f232c3ad87989f8ee46b95
gucipantofi123 <bgtrak@outlook.com>
2024-11-28 16:34:05 +0200

    Update ZigzagConversion.js

--- ZigzagConversion.js
+++ ZigzagConversion.js
@@ -1,5 +1,16 @@
 function ZigzagConversion(s, numRows){
-
+if(numRows === 1) return s;
+    let result = "";
+    let step = 2 * numRows - 2;
+    for(let i = 0; i < numRows; i++){
+        for(let j = i; j < s.length; j += step){
+            result += s[j];
+            if(i !== 0 && i !== numRows - 1 && j + step - 2 * i < s.length){
+                result += s[j + step - 2 * i];
+            }
+        }
+    }
+    return result;
 }
 
 module.exports = ZigzagConversion;
